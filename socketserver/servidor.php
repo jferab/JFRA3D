91d5776f51aa1c41ef77e688a7a729e6d4952908
jferab <jferab@gmail.com>
2019-04-22 20:35:28 +0200

    20190422

--- socketserver/servidor.php
+++ socketserver/servidor.php
@@ -8,10 +8,11 @@ class echoServer extends WebSocketServer {
         $this->usuario[$user->id]['utc'] = date('U');
         $explotado = explode("|", $message);
         // Creamos la columna mensaje que contiene el mensaje completo a reenviar
-        $this->usuario[$user->id]['idJugador'] = $user->id;    
+        $this->usuario[$user->id]['idJugador'] = $user->id;
         $this->usuario[$user->id]['posX'] = explode(",",$explotado[0])[1];                      // Posicion X
         $this->usuario[$user->id]['posY'] = explode(",",$explotado[0])[2];                      // Posicion Y
 
+        //$longaniza = $user->id."&";
         $longaniza = "";
         foreach ($this->usuario as &$valor) {
             if($valor['utc'] > date('U') - 15){
