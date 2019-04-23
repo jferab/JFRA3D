454f2d7d124e6c36a20f5f52df26035e9d42ced4
Raulrowhat <36578976+Raulrowhat@users.noreply.github.com>
2019-04-23 01:02:56 -0500

    Add files via upload

--- socketserver/servidor.php
+++ socketserver/servidor.php
@@ -12,8 +12,8 @@ class echoServer extends WebSocketServer {
         $this->usuario[$user->id]['posX'] = explode(",",$explotado[0])[1];                      // Posicion X
         $this->usuario[$user->id]['posY'] = explode(",",$explotado[0])[2];                      // Posicion Y
 
-        //$longaniza = $user->id."&";
-        $longaniza = "";
+         //$longaniza = "";//cambio Raul 23/04/2019
+		$longaniza = $user->id."|";
         foreach ($this->usuario as &$valor) {
             if($valor['utc'] > date('U') - 15){
                $longaniza .= $valor['idJugador'].",".$valor['posX'].",".$valor['posY']."|";
